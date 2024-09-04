95982a0ef0a8819eb998239c84e28badde60e193
Axel Velasquez <yoloax11@gmail.com>
2024-09-03 23:27:45 -0700

    modified the index to have the authorization context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App'; 
 import { Provider } from 'react-redux';
 import { store } from './redux/redux-store';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <Provider store = {store}>
       <App />  
     </Provider>
+    </AuthContextProvider>
   </React.StrictMode>
+
 );
    
