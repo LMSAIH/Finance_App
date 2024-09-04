63314f4f8dee9154e535b8b78d0a32812b679c53
Axel Velasquez <yoloax11@gmail.com>
2024-09-03 23:26:42 -0700

    updated the routes to implement the authorization

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const Router = express.Router();
 const FinanceController = require('../controllers/FinanceController');
+const {requireAuth} = require('../middleware/RequireAuth');
 
+//Ready to go into the app, as long as the integration with the frontend is complete
+//Router.use(requireAuth);
 //gets all the finance information related to a user
 Router.get("/", FinanceController.getAll);
 
