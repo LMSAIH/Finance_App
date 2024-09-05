de1f4a38feac170d93979672029edcbe56e5799b
Axel Velasquez <yoloax11@gmail.com>
2024-09-04 23:35:55 -0700

    reworked the entire login page to be cmopatible with the api

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,54 +1,48 @@
-import React, { useEffect } from 'react';
-import { getFinancesTC } from '../redux/finance-reducer';
-import { useDispatch, useSelector } from 'react-redux';
-import { getFinancesData } from './../redux/HomeSelector';
-import { Formik, Form, Field } from 'formik';
-import { loginTC } from '../redux/auth-reducer';
+import { useState } from "react";
+import  { useAuthContext }  from '../hooks/useAuthContext';
+import { useNavigate } from 'react-router-dom';
 
+import axios from 'axios';
 
+const LoginPage = () => {
 
-export const LoginPage = () => {
-    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const {dispatch} = useAuthContext();
+    const navigate = useNavigate();
 
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setIsLoading(true);
+        
+        axios.post('http://localhost:4000/api/user/login', {email, password})
+        .then((response) => {
+            localStorage.setItem('user', JSON.stringify(response.data));
+            setIsLoading(false);
+            dispatch({type: 'LOGIN', payload: response.data});
+            navigate('/');
+        })
+        .catch((err) => {
+            setError(err.response.data.error);
+            setIsLoading(false);
+        });
 
-    const loginUser = (login, password, rememberMe) => {
-        dispatch(loginTC(login, password, rememberMe))
     }
 
-    
-    console.log("HEEEEEEEERREEEEEEEEEE");
-  return (
-    <div>
-        <h1>LOGIN PAGE</h1>
-        <LoginForm loginUser = {loginUser}/>
-    </div>
-  )
-}
-
-const LoginForm = (props) => {
-    let submit = (values) => {
-        props.loginUser(values.login, values.password, values.rememberMe)
-
-    }
     return (
-        <div>
-            <Formik
-                enableReinitialize
-                initialValues={{ login: "Login", password: "Password", rememberMe: false }}
-                onSubmit={submit}>
-
-                <Form>
-                    <Field type="string" name="login"/>
-                    <div>
-                        <Field type="password" name="password"/>
-                    </div>
-                    
-                    <Field type="checkbox" name="rememberMe"/>
-                    <button type="submit">
-                        Submit
-                    </button>
-                </Form>
-            </Formik>
+        <div className = "loginform">
+            <form onSubmit = {handleSubmit}> 
+                <label>Email: </label>
+                <input type = "email" onChange={(e)=>{setEmail(e.target.value)}} name="email" value={email}></input> 
+                <label>Password: </label>
+                <input type = "password" onChange={(e)=>{setPassword(e.target.value)}} name="password" value={password}></input> 
+                <button disabled = {isLoading}> Submit </button>
+                <div className = "error"> {error} </div>
+            </form>
         </div>
-    );
-};
+      );
+}
+ 
+export default LoginPage;
