834bbdedf917a064f8b433fdd0e34e471effe592
Axel Velasquez <yoloax11@gmail.com>
2024-09-04 23:36:42 -0700

    reworked the navigation to include the username and to also have a logout functionality, this functiontality needs to also have a clear for the user objects in the future, but this will be done when the finances context is properly set up

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,38 +1,36 @@
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+  const { user, dispatch } = useAuthContext();
 
-    const userName = useSelector((state) => {
-        return state.auth.login
-    })
+  const logout = () => {
+    if (user) {
+      localStorage.removeItem("user");
+      dispatch({ type: "LOGOUT" });
+      navigate("/login");
+    }
+  };
 
-    const email = useSelector((state) => {
-        return state.auth.email
-    })
-
-    return (
-      <nav>
-        <div>
-            {userName}
-        </div>
-        <div>
-            {email}
-        </div>
-        <hr/>
-        <div>
-          <Link to='/'>Home</Link>
-        </div>
-        <div>
-          <Link>Landing page</Link>
-        </div>
-        <div>
-          <Link to='/login'>Login</Link>
-        </div>
-        <div>
-          <Link to='/signup'>Sign up</Link>
-        </div>
-      </nav>
-    )
-  }
+  return (
+    <nav>
+      <div>{(user ? user.email : "Not logged in")}</div>
+      <hr />
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link>Landing page</Link>
+      </div>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+      <div>
+        <Link to="/signup">Sign up</Link>
+      </div>
+      <button onClick={logout}>logout</button>
+    </nav>
+  );
+};
