88a7331a28d0de8e0b07466b7ed0c1a0f6b09866
Axel Velasquez <yoloax11@gmail.com>
2024-09-04 23:35:20 -0700

    updated the app to redirect dynamically based on if the user is logged in or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Router, Routes, Navigate } from 'react-router-dom';
 import { Home } from './components/Home/Home';
-import { LoginPage } from './components/LoginPage';
+import  LoginPage  from './components/LoginPage';
 import { Navbar } from './components/Navbar';
 import SignUpPage from './components/SignUpPage';
+import { useAuthContext } from './hooks/useAuthContext';
 
 
 function App() {
+
+  const {user} = useAuthContext();
+  
   return (
     <BrowserRouter>
       <div className="Wrapper">
@@ -18,7 +22,7 @@ function App() {
           <Routes>
             <Route path='/login' element={<LoginPage />} />
             <Route path = '/signup' element={<SignUpPage />} />
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={user ? <Home /> : <Navigate to="/login" />}/>
           </Routes>
         </div>
       </div>
