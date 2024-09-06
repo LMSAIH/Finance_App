e8960bf62fe6e022ce2a36d9fb159b9df5dd25f7
Axel Velasquez <yoloax11@gmail.com>
2024-09-05 17:33:44 -0700

    modified the base router to have access to the api and include authorization headers

--- frontend/src/components/routers/base-router.js
+++ frontend/src/components/routers/base-router.js
@@ -1,38 +1,58 @@
-import axios from 'axios';
-
-
-
+import axios from "axios";
 
 const instance = axios.create({
-    withCredentials: false,
-    baseURL: 'http://localhost:4000',
-  });
+  withCredentials: false,
+  baseURL: "http://localhost:4000",
+});
 
-  export const FinancesAPI  = {
-    
-    loginNewUser (login, password, rememberMe) {
-        return instance.post('/api/users', {
-            login: login,
-            password: password,
-            rememberMe: rememberMe
-        }).then(res => res.body);
-    },
-    getFinances() {
-        return instance.get("/api/finance/").then(res => res.data);
-    },
-    createNewFinance(year, month, userId, income, concept, amount) {
-        return instance.post("/api/finance", {
-            year: year,
-            month: month,
-            user_id: userId,
-            income: income,
-            outcome: [{
-                concept: concept,
-                amount: amount
-            }]
-        }).then(res => res.data);
-    },
-    deleteFinance(id) {
-        return instance.delete("/api/finance/" + id).then(res => res.data);
-    }
-  } 
+export const FinancesAPI = {
+  loginNewUser(login, password, rememberMe) {
+    return instance
+      .post("/api/users", {
+        login: login,
+        password: password,
+        rememberMe: rememberMe,
+      })
+      .then((res) => res.body);
+  },
+  getFinances(token) {
+    console.log(`in get finances: ${token}`);
+    return instance
+      .get("/api/finance/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data);
+  },
+  createNewFinance(year, month, userId, income, concept, amount, token) {
+    return instance
+      .post("/api/finance",  {
+        year: year,
+        month: month,
+        user_id: userId,
+        income: income,
+        outcome: [
+          {
+            concept: concept,
+            amount: amount,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+    })
+      .then((res) => res.data);
+  },
+  deleteFinance(id, token) {
+    return instance
+      .delete("/api/finance/" + id, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data);
+  },
+};
