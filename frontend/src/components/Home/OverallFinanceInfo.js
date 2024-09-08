6ad1b301bcc5d2d58849b1584dcbb60629019709
Axel Velasquez <yoloax11@gmail.com>
2024-09-08 16:58:32 -0700

    modified the component to make it fit for the UI, functionality is still there

--- frontend/src/components/Home/OverallFinanceInfo.js
+++ frontend/src/components/Home/OverallFinanceInfo.js
@@ -2,10 +2,12 @@ import PieChart from "../Graphs/PieGraph"
 
 export const OverallFinanceInfo = (props) => {
     
-    return (<div>
-        <h4>Overall info about this period of time:</h4>
-        <div>Total income: {props.totalIncome}</div>
-        <div>Total outcome: {props.totalOutcome}</div>
+    return (<div className = "thisMonth">
+        <div className = "thisMonthInfo">
+        <h4>This month's summary: </h4>
+        <p>Total income: {props.totalIncome}</p>
+        <p>Total expenses: {props.totalOutcome}</p>
+        </div>
         <PieChart income = {props.totalIncome} outcome = {[{concept: "Overall", amount: props.totalOutcome}]}/>
     </div>)
 }
