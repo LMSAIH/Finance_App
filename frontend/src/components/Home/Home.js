cb714849d5e7c6adad35a35824ad939d42b8146c
Axel Velasquez <yoloax11@gmail.com>
2024-09-10 20:53:29 -0700

    updated the homepage to accommodate for styles

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,130 +1,128 @@
-import React, { useEffect, useState, version } from 'react';
-import el from './Home.module.css'
-import { getFinancesData } from '../../redux/HomeSelector';
-import { useDispatch, useSelector } from 'react-redux';
-import { getFinancesTC, createNewFinanceTC } from '../../redux/finance-reducer';
-import { deleteFinanceTC } from '../../redux/finance-reducer';
-import { FinanceInfo } from './FinanceInfo';
-import { CreateNewFinanceForm } from '../../forms/CreateNewFinanceForm';
-import { SelectIFilterForm } from '../../forms/SelectFilterForm';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import { OverallFinanceInfo } from './OverallFinanceInfo';
+import React, { useEffect, useState, version } from "react";
+import el from "./Home.module.css";
+import { getFinancesData } from "../../redux/HomeSelector";
+import { useDispatch, useSelector } from "react-redux";
+import { getFinancesTC, createNewFinanceTC } from "../../redux/finance-reducer";
+import { deleteFinanceTC } from "../../redux/finance-reducer";
+import { FinanceInfo } from "./FinanceInfo";
+import { CreateNewFinanceForm } from "../../forms/CreateNewFinanceForm";
+import { SelectIFilterForm } from "../../forms/SelectFilterForm";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { OverallFinanceInfo } from "./OverallFinanceInfo";
 
 //Not finished components:
 // import { AmountOutcomesEdditingForm } from './AmountOutcomesEdditingForm';
 // import { OutcomesForm } from './OutcomesForm';
 
-
 export const Home = () => {
+  const [isEdditting1, setIsEdditing1] = useState(false);
+  const [filter, setFilter] = useState({ month: "january", year: 2024 });
+  const [finData, setFinData] = useState(null);
+  const [totalOutcome, setTotalOutcome] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
 
-    const [isEdditting1, setIsEdditing1] = useState(false);
-    const [filter, setFilter] = useState({ month: "january", year: 2024 });
-    const [finData, setFinData] = useState(null);
-    const [totalOutcome, setTotalOutcome] = useState(0);
-    const [totalIncome, setTotalIncome] = useState(0);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    let financesData = useSelector(getFinancesData);
+  let financesData = useSelector(getFinancesData);
 
+  const { user } = useAuthContext();
 
-    const {user} = useAuthContext();
+  const token = user.token;
 
-    const token = user.token;
+  const getFinances = (token) => {
+    dispatch(getFinancesTC(token));
+  };
 
-    const getFinances = (token) => {
-    
-        dispatch(getFinancesTC(token))
-    }
+  const createNewFinance = (year, month, income, outcomes, token) => {
+    dispatch(createNewFinanceTC(year, month, income, outcomes, token));
+  };
 
-    const createNewFinance = (year, month, income, outcomes, token) => {
-        dispatch(createNewFinanceTC(year, month, income, outcomes, token))
-    }
+  const deleteFinance = (id, token) => {
+    dispatch(deleteFinanceTC(id, token));
+  };
 
-    const deleteFinance = (id, token) => {
-        dispatch(deleteFinanceTC(id, token));
-    }
+  useEffect(() => {
+    getFinances(token);
+  }, [dispatch]);
 
-    
-    useEffect(() => {
-        getFinances(token);
-    }, [dispatch]);
-
-    useEffect(() => {
-        let newFinanceData;
-        let finalOutcome = 0;
-        let finalIncome = 0;
-        if (financesData.length > 0) {
-            newFinanceData = (financesData.filter(e => {
-                if (e.month === filter.month && e.year === filter.year)
-                {
-                    let tempOutcome = 0;
-                    finalIncome += e.income;
-                    e.outcome.forEach(outcome => {
-                        tempOutcome += outcome.amount;
-                    });;
-                    finalOutcome += tempOutcome;
-                    return true;
-                }
-                    
-            }))
+  useEffect(() => {
+    let newFinanceData;
+    let finalOutcome = 0;
+    let finalIncome = 0;
+    if (financesData.length > 0) {
+      newFinanceData = financesData.filter((e) => {
+        if (e.month === filter.month && e.year === filter.year) {
+          let tempOutcome = 0;
+          finalIncome += e.income;
+          e.outcome.forEach((outcome) => {
+            tempOutcome += outcome.amount;
+          });
+          finalOutcome += tempOutcome;
+          return true;
         }
-        setTotalIncome(finalIncome);
-        setTotalOutcome(finalOutcome);
-        setFinData(newFinanceData);
-        if (newFinanceData) {
-            if (newFinanceData.length == 0) {
-                setFinData(null);
-            }
-        }
-        
-    }, [financesData, filter]);
-
-
-
-    const createNewIncome = () => {
-        setIsEdditing1(true);
+      });
     }
-
-    const disableNewIncome = () => {
-        setIsEdditing1(false)
+    setTotalIncome(finalIncome);
+    setTotalOutcome(finalOutcome);
+    setFinData(newFinanceData);
+    if (newFinanceData) {
+      if (newFinanceData.length == 0) {
+        setFinData(null);
+      }
     }
+  }, [financesData, filter]);
 
+  const createNewIncome = () => {
+    setIsEdditing1(true);
+  };
 
-    return (
+  const disableNewIncome = () => {
+    setIsEdditing1(false);
+  };
+
+  return (
+    <div>
+      <OverallFinanceInfo
+        totalOutcome={totalOutcome}
+        totalIncome={totalIncome}
+      />
+      <div className="createNew">
         <div>
-            <OverallFinanceInfo totalOutcome = {totalOutcome} totalIncome = {totalIncome} />
-            
-            <div className = "financialInfo">
-                <div>
-                    <SelectIFilterForm setFilter={setFilter} />
-                </div>
-                {(finData) ? (finData.map(e => <FinanceInfo financeData={e} deleteFinance={deleteFinance} token={token} />)) : (
-                    <div>Loading...</div>
-                )}
-                <div>
-                    <div>
-                        {isEdditting1 ? <div>
-                            <CreateNewFinanceForm createNewFinance={createNewFinance} token={token} />
-                            <button onClick={disableNewIncome}>Stop creating an new finance object for a month and a year</button>
-                        </div> : <button onClick={createNewIncome}>Create finance object for a month and a year</button>}
-                    </div>
-                </div>
+          {isEdditting1 ? (
+            <div>
+              <CreateNewFinanceForm
+                createNewFinance={createNewFinance}
+                token={token}
+              />
+              <button onClick={disableNewIncome}>
+                Stop creating an new finance object for a month and a year
+              </button>
             </div>
+          ) : (
+            <button className="openCreateBtn" onClick={createNewIncome}>
+              Create finance object for a month and a year
+            </button>
+          )}
         </div>
+      </div>
+      <div className="filter">
+        <SelectIFilterForm setFilter={setFilter} />
+      </div>
 
-
-    )
-}
-
-
-
-
-
-
-
-
-
-
-
-
+      <div className="financialInfo">
+        {finData ? (
+          finData.map((e) => (
+            <FinanceInfo
+              financeData={e}
+              deleteFinance={deleteFinance}
+              token={token}
+              key={e._id}
+            />
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
+};
