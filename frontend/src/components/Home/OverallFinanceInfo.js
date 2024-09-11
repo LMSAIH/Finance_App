b39bde34f51cade456b5e03c61732c329ffb4aa8
Axel Velasquez <yoloax11@gmail.com>
2024-09-10 20:53:42 -0700

    added classes to overall finances

--- frontend/src/components/Home/OverallFinanceInfo.js
+++ frontend/src/components/Home/OverallFinanceInfo.js
@@ -4,9 +4,9 @@ export const OverallFinanceInfo = (props) => {
     
     return (<div className = "thisMonth">
         <div className = "thisMonthInfo">
-        <h4>This month's summary: </h4>
-        <p>Total income: {props.totalIncome}</p>
-        <p>Total expenses: {props.totalOutcome}</p>
+        <h4 className="summary">This month's summary: </h4>
+        <p className = "info">Total income: {props.totalIncome}</p>
+        <p className = "expenses">Total expenses: {props.totalOutcome}</p>
         </div>
         <PieChart income = {props.totalIncome} outcome = {[{concept: "Overall", amount: props.totalOutcome}]}/>
     </div>)
