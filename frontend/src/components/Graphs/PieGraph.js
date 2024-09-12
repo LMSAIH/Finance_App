00f726b1c038ef9a31f9c010f2b69af6fa3a41b6
Axel Velasquez <yoloax11@gmail.com>
2024-09-12 15:25:36 -0700

    modified the piegraph to account for negative saving scenarios

--- frontend/src/components/Graphs/PieGraph.js
+++ frontend/src/components/Graphs/PieGraph.js
@@ -42,9 +42,15 @@ const PieChart = ({ income, outcome }) => {
     const spending = amounts.reduce((sum, amount) => sum + amount, 0);
     console.log(spending);
     setTotalSpending(spending);
-    setToSave(income - spending);
+
+    if (totalSpending > income) {
+      setToSave(0);
+    } else {
+      setToSave(income - spending);
+    }
+
     console.log("Tosave: ", income - spending);
-  }, [income, outcome]);
+  }, [income, outcome, totalSpending]);
 
   const pieChartData = {
     labels: outcomeConcepts,
@@ -64,6 +70,33 @@ const PieChart = ({ income, outcome }) => {
   return (
     <div className="PieChart">
       <h3>The income for this month: {income}</h3>
+      <div className="upDown">
+        {totalSpending < income && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="green"
+            className="bi bi-caret-up-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+          </svg>
+        )}
+        {income < totalSpending && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="red"
+            className="bi bi-caret-down-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+          </svg>
+        )}
+        {Math.abs(income - totalSpending)}
+      </div>
       {/* there was a delete button, but i decided to seperate functionality in wrapper component, if you have any concerns text me */}
       <Pie options={options} data={pieChartData} />
     </div>
