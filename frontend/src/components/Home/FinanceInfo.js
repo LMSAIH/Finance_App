3c85f20cd15677434a61e654a2b4cf2edba54c86
Axel Velasquez <yoloax11@gmail.com>
2024-09-12 15:25:47 -0700

    modified financeinfo to fit the UI design

--- frontend/src/components/Home/FinanceInfo.js
+++ frontend/src/components/Home/FinanceInfo.js
@@ -1,48 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import PieGraph from '../Graphs/PieGraph';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import PieGraph from "../Graphs/PieGraph";
+import { useNavigate } from "react-router-dom";
 
 export const FinanceInfo = (props) => {
+  console.log(props.financeData);
+  console.log("HERE");
+  const navigate = useNavigate();
 
-    console.log(props.financeData);
-        console.log("HERE");
-    const navigate = useNavigate();
+  const deleteFinance = () => {
+    props.deleteFinance(props.financeData._id, props.token);
+  };
 
-    const deleteFinance = () => {
-        props.deleteFinance(props.financeData._id, props.token)
-    }
+  let [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    const newTotalAmount = props.financeData.outcome.reduce(
+      (sum, e) => sum + e.amount,
+      0
+    );
+    setTotalAmount(newTotalAmount);
+  }, [props.financeData.outcome]);
 
-    let [totalAmount, setTotalAmount] = useState(0);
-    useEffect(() => {
-        const newTotalAmount = props.financeData.outcome.reduce((sum, e) => sum + e.amount, 0);
-        setTotalAmount(newTotalAmount);
-        
-    }, [props.financeData.outcome]);
-
-    return (
-        <div className='FinanceInfo'>
-            <div>Year: {props.financeData.year}</div>
-            <div>Month: {props.financeData.month}</div>
-            <div>Income: {props.financeData.income}</div>
-            <div>
-                Outcome: {props.financeData.outcome.map(e => {
-                    return (<div>
-                        <div>{e.concept}: {e.amount}</div>
-                    </div>
-                    )
-                })}
-            </div>
-            <div>
-            <PieGraph income={props.financeData.income} outcome={props.financeData.outcome} />
-            </div>
-            <div>
-                <button onClick={deleteFinance}>Delete finance</button>
-                <button onClick={() => {navigate(`/change/${props.financeData._id}`)}}>Change finance</button>
-            </div>
+  return (
+    <div className="FinanceInfo">
+      <div className="financialContent">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-calendar-range"
+            viewBox="0 0 16 16"
+          >
+            <path d="M9 7a1 1 0 0 1 1-1h5v2h-5a1 1 0 0 1-1-1M1 9h4a1 1 0 0 1 0 2H1z" />
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+          </svg>{" "}
+          {props.financeData.month} {props.financeData.year}
         </div>
-    )
-}
 
-
-
-
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-wallet2"
+            viewBox="0 0 16 16"
+          >
+            <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z" />
+          </svg>{" "}
+          {props.financeData.income}
+        </div>
+      </div>
+      <div>
+        <PieGraph
+          income={props.financeData.income}
+          outcome={props.financeData.outcome}
+        />
+      </div>
+      <div className = "actionButtons">
+        <button onClick={deleteFinance}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-trash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+          </svg>
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/change/${props.financeData._id}`);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-toggles2"
+            viewBox="0 0 16 16"
+          >
+            <path d="M9.465 10H12a2 2 0 1 1 0 4H9.465c.34-.588.535-1.271.535-2s-.195-1.412-.535-2" />
+            <path d="M6 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6m0 1a4 4 0 1 1 0-8 4 4 0 0 1 0 8m.535-10a4 4 0 0 1-.409-1H4a1 1 0 0 1 0-2h2.126q.138-.534.41-1H4a2 2 0 1 0 0 4z" />
+            <path d="M14 4a4 4 0 1 1-8 0 4 4 0 0 1 8 0" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
