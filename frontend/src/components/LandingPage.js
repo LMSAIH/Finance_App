0ffe0870a3504256b7559249ddc0efe2eacb438c
Axel Velasquez <yoloax11@gmail.com>
2024-09-17 20:58:34 -0700

    added a few spans to the landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,45 +1,46 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Landing from './LandingPage.css';
-import logo from './images/image.png';
-import LandingPageGraph from './Graphs/LandingPageGraph';
-
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Landing from "./LandingPage.css";
+import logo from "./images/image.png";
+import LandingPageGraph from "./Graphs/LandingPageGraph";
 
 const LandingPage = () => {
-    return ( 
-       
-            <div className="wrapper">
-        
-                <nav className="navbar">
-                    <img src={logo} alt="Logo" height="100px" />
-        
-                    <div className="spread">
-                            
-                        <ul className="navlist">
-                            <li><a href='/login'>Login</a></li>
-                            <li><a href='/signup'>Sign Up</a></li>
-                        </ul>
-                        
-        
-                    </div>
-                </nav>
-        
-                <div className="workspace">
-                    <div className="container">
-                        <h2>Discover how you can manage all your finances with an application that can take care of organizing it all for you.</h2>
-                        <h1>Welcome to Finance App.</h1>
-                        <h3>Your path for success.</h3>
-                    </div>
+  return (
+    <div className="wrapper">
+      <nav className="navbar">
+        <img src={logo} alt="Logo" height="100px" />
 
-                    <div className="container2">
-                        <LandingPageGraph />
-                    </div>
-                </div>
-                
-            </div>
+        <div className="spread">
+          <ul className="navlist">
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/signup">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-     );
-}
- 
+      <div className="workspace">
+        <div className="container">
+          <h2>
+            {" "}
+            <span className = "discover">Discover</span> how you can <span className = "discover">manage</span> all your finances with an
+            application that can <span className = "discover"> take care  </span>of organizing it all for 
+            <span className="you"> you</span>.
+          </h2>
+          <h1>Welcome to Finance App.</h1>
+          <h3>Your path for success.</h3>
+        </div>
+
+        <div className="container2">
+          <LandingPageGraph />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default LandingPage;
