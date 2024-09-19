06fb0195ed93f19a5c8157be284d0ca28dddb09d
Axel Velasquez <yoloax11@gmail.com>
2024-09-18 20:39:31 -0700

    fixed the error handling in the signup page

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -1,49 +1,67 @@
 import { useState } from "react";
-import  { useAuthContext }  from '../hooks/useAuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const {dispatch} = useAuthContext();
-    const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        setIsLoading(true);
-        
-        axios.post('http://localhost:4000/api/user/signup', {email, password})
-        .then((response) => {
-            localStorage.setItem('user', JSON.stringify(response.data));
-            setIsLoading(false);
-            dispatch({type: 'LOGIN', payload: response.data});
-            navigate('/');
-        })
-        .catch((err) => {
-            setError(err.response.data.error);
-            setIsLoading(false);
-        });
+    axios
+      .post("http://localhost:4000/api/user/signup", { email, password })
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        setIsLoading(false);
+        dispatch({ type: "LOGIN", payload: response.data });
+        navigate("/");
+      })
+      .catch((err) => {
+        setError(err.response.data.error);
+        setIsLoading(false);
+      });
+  };
 
-    }
+  return (
+    <div className="signupform">
+      <h4 className="formTitle">
+        Sign<span className="up">Up</span>
+      </h4>
+      <form onSubmit={handleSubmit}>
+        <label>Email </label>
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          name="email"
+          value={email}
+        ></input>
+        <label>Password </label>
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          name="password"
+          value={password}
+        ></input>
+        <button disabled={isLoading} className="submitBtn">
+          {" "}
+          Sign Up!{" "}
+        </button>
+        {error && <div className="error"> {error} </div>}
+      </form>
+    </div>
+  );
+};
 
-    return (
-        <div className = "signupform">
-            <h4 className="formTitle">Sign<span className = "up">Up</span></h4>
-            <form onSubmit = {handleSubmit}> 
-                <label>Email </label>
-                <input type = "email" onChange={(e)=>{setEmail(e.target.value)}} name="email" value={email}></input> 
-                <label>Password </label>
-                <input type = "password" onChange={(e)=>{setPassword(e.target.value)}} name="password" value={password}></input> 
-                <button disabled = {isLoading} className="submitBtn"> Sign Up! </button>
-                <div className = "error"> {error} </div>
-            </form>
-        </div>
-      );
-}
- 
 export default SignUpPage;
